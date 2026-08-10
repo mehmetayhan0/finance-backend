7cfc2db6e817047f5a3aca409719d3e16365352f
mehmetayhan0 <mehmetayhnn@gmail.com>
2026-08-10 03:45:06 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,23 @@
-const CACHE_NAME = 'finans-v1';
+const CACHE_NAME = 'finansim-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-512.png'
+];
 
 self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });

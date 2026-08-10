239090e72eca210e749e4a17dc5ce0e50f7e6699
mehmetayhan0 <mehmetayhnn@gmail.com>
2026-08-10 04:07:20 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 const CACHE_NAME = 'finansim-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-512.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
